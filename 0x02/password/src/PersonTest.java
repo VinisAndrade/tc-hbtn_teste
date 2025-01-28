56fb587f71ea8de9734903a8cc5c248d8e309fbd
Vinicius Santos Andrade <viniciusarmani@gmail.com>
2025-01-28 17:27:33 -0300

    Update PersonTest.java

--- 0x02/password/src/PersonTest.java
+++ 0x02/password/src/PersonTest.java
@@ -1,7 +1,4 @@
-package src;
-
 import org.junit.jupiter.api.BeforeAll;
-    
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -18,38 +15,38 @@ public class PersonTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "PaulMcCartney2", "NeilArms2" })
-    void check_user_valid(String username) {
+    @ValueSource(strings = {"PaulMcCartney2", "NeilArms2"})
+    public void check_user_valid(String username) {
         assertTrue(person.checkUser(username));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "Paul#McCartney", "Neil@Arms" })
-    void check_user_not_valid(String username) {
+    @ValueSource(strings = {"Paul#McCartney", "Neil@Arms"})
+    public void check_user_not_valid(String username) {
         assertFalse(person.checkUser(username));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "123456789", "#$%1234" })
-    void does_not_have_letters(String password) {
+    @ValueSource(strings = {"123456789", "#$%1234"})
+    public void does_not_have_letters(String password) {
         assertFalse(person.checkPassword(password));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "Abcabcdefgh@", "#hbtn@%tc" })
-    void does_not_have_numbers(String password) {
+    @ValueSource(strings = {"Abcabcdefgh@", "#hbtn@%tc"})
+    public void does_not_have_numbers(String password) {
         assertFalse(person.checkPassword(password));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "Abc@123", "12$@hbt" })
-    void does_not_have_eight_chars(String password) {
+    @ValueSource(strings = {"Abc@123", "12$@hbt"})
+    public void does_not_have_eight_chars(String password) {
         assertFalse(person.checkPassword(password));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "abC123456$", "Hbtn@1234", "Betty@1#2", "Hbtn@123" })
-    void check_password_valid(String password) {
+    @ValueSource(strings = {"abC123456$", "Hbtn@1234", "Betty@1#2", "Hbtn@123"})
+    public void check_password_valid(String password) {
         assertTrue(person.checkPassword(password));
     }
 }
